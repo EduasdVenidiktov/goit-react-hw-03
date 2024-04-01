e08a8fa66e877949936e709095a5fab7255ccdf0
EduardVenidiktov <edven7777777@gmail.com>
2024-04-01 12:33:20 +0300

    tasks 1-5

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,47 @@
-import { useState, useEffect } from "react";
-import Feedback from "./components/Feedback/Feedback";
-import Options from "./components/Options/Options";
-import Description from "./components/Description/Description";
-import Notification from "./components/Notification/Notification";
+import { useState, useEffect, useCallback } from "react";
+
+import ContactForm from "./components/ContactForm/ContactForm";
+import SearchBox from "./components/SearchBox/SearchBox";
+import ContactList from "./components/ContactList/ContactList";
+
+import useContact from "../useContact.json";
 import css from "./App.module.css";
 
-function App() {
-  // Стан для зберігання типів відгуків
-  const [feedbackTypes, setFeedbackTypes] = useState(() => {
-    const savedFeedback = JSON.parse(localStorage.getItem("feedbackTypes"));
-    return (
-      savedFeedback || {
-        good: 0,
-        neutral: 0,
-        bad: 0,
-      }
-    );
+export default function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+    return savedContacts || useContact;
   });
 
-  // Функція для оновлення стану відгуку
-  const updateFeedback = (feedbackType) => {
-    setFeedbackTypes((prevTypes) => ({
-      ...prevTypes,
-      [feedbackType.toLowerCase()]: prevTypes[feedbackType.toLowerCase()] + 1,
-    }));
-  };
+  // Функція для фільтрації контактів за ім'ям
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
-  // Функція для скидання зібраних відгуків
-  const resetFeedback = () =>
-    setFeedbackTypes({
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    });
+  // Функція для видалення контакту, обгорнута в useCallback
+  const deleteContact = useCallback((id) => {
+    setContacts((prevContacts) =>
+      prevContacts.filter((contact) => contact.id !== id)
+    );
+  }, []); // Залежності не вказані, оскільки функція не має зовнішніх залежностей
+
+  // Функція для додавання нового контакту
+  const addContact = useCallback((newContact) => {
+    setContacts((prevContacts) => [...prevContacts, newContact]);
+  }, []);
+
   // Ефект для збереження стану в локальне сховище
   useEffect(() => {
-    localStorage.setItem("feedbackTypes", JSON.stringify(feedbackTypes));
-  }, [feedbackTypes]);
-
-  const options = ["Good", "Neutral", "Bad", "Reset"];
-
-  // Обчислення загальної кількості відгуків
-  const totalFeedback =
-    feedbackTypes.good + feedbackTypes.neutral + feedbackTypes.bad;
-
-  const positiveFeedback = Math.round(
-    (feedbackTypes.good / totalFeedback) * 100
-  );
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className={css.container}>
-      <Description />
-      {/* Передача опцій, функції оновлення стану та функції скидання відгуків */}
-      <Options
-        options={options}
-        updateFeedback={updateFeedback}
-        resetFeedback={resetFeedback}
-        totalFeedback={totalFeedback}
-      />
-
-      {/* якщо натиснута хоч одна кнопка, то з'являються feedbackTypes, інакше
-      повідомлення */}
-      {totalFeedback > 0 ? (
-        <Feedback
-          feedbackTypes={feedbackTypes}
-          totalFeedback={totalFeedback}
-          positiveFeedback={positiveFeedback}
-        />
-      ) : (
-        <Notification />
-      )}
+      <h1 className={css.title}>Phonebook</h1>
+      <ContactForm onAddContact={addContact} />
+      <SearchBox value={searchTerm} onChange={setSearchTerm} />
+      <ContactList contacts={filteredContacts} onDelete={deleteContact} />
     </div>
   );
 }
-
-export default App;
--- src/components/Notification/Notification.jsx
+++
@@ -1,9 +0,0 @@
-import css from "./Notification.module.css";
-
-const Notification = () => (
-  <p className={css.feedbackText}>
-    No feedback yet. Please leave your feedback!
-  </p>
-);
-
-export default Notification;
